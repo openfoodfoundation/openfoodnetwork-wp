e3f7bbf4c7a7a3417a3f48440ff831a0133a1aad
summerscope <summerscope@gmail.com>
2015-05-05 17:37:50 +1000

    WIP styling and customisation for child theme

---
+++ wp-content/themes/theblog-child/html/single/content-page.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package cactus
+ */
+?>
+
+<?php if(!is_front_page() && !is_page_template('page-templates/front-page.php')):?>
+    <div class="title-content">
+        <div class="text-2 font-1">
+            <div>
+                <h1><?php the_title(); ?></h1>
+            </div>
+        </div>
+    </div><!-- .title-content -->
+<?php endif;?>
+
+<div class="single-page-content"><!--Single Page Content-->
+    
+    <article>
+            <div class="body-content">
+            <?php the_content(); ?>
+            </div>
+             <?php
+				global $page, $numpages;
+				wp_link_pages(array(
+					'before' => '<div class="cactus-post-break font-1"><span class="total-post"> '. esc_html__('Page ','cactusthemes').$page.esc_html__(' of ','cactusthemes').$numpages.' </span><span class="page-link-wp">',
+					'after' => '</span></div>',
+					'next_or_number' => 'next',
+					'nextpagelink' => '<span class="next-post-link"><i class="fa fa-angle-right"></i></span>',
+					'separator'        => '<span class="current">'. $page.'</span>',
+					'previouspagelink' => '<span class="previous-post-link"><i class="fa fa-angle-left"></i></span>' ,
+					'pagelink' => '%',
+					'echo' => 1 )
+				);  
+			?>
+            <?php $disable_comments =ot_get_option('disable_comments');?>
+			<?php if($disable_comments != 'on'):?>
+                <div class="comment-form-fix">
+                    <?php
+                        if ( comments_open() || '0' != get_comments_number() )
+                            comments_template();
+                    ?>
+                </div>
+            <?php endif;?>
+	</article>
+        
+</div><!-- .single-page-content -->
