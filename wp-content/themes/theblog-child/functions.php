83a55029d47e53b73c4ae0fd5a6b59e3cb3d29dd
summerscope <summerscope@gmail.com>
2015-05-06 14:14:47 +1000

    Remove this from here cus no!

--- wp-content/themes/theblog-child/functions.php
+++ wp-content/themes/theblog-child/functions.php
@@ -1,5 +1,6 @@
 <?php
 	
+// This is best practice stuff:	
 // add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 // 	function theme_enqueue_styles() {
 //     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '../theblog/style.css' );
@@ -10,18 +11,3 @@
 // 	}
 
 
-// Add Your Menu Locations
-function register_my_menus() {
-  register_nav_menus(
-    array(  
-    	'user_guide' => __( 'User Guide' ), 
-    )
-  );
-} 
-add_action( 'init', 'register_my_menus' );
-
-function default_user_guide() { // HTML markup for a default message in menu location
-	echo "<ul class='nav'>					
-		<li>User Guide</li>
-	</ul>"
-}
