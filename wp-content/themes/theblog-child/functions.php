8426acb3f1284c271c412f897f634d58d8b8636e
summerscope <summerscope@gmail.com>
2015-05-06 14:41:02 +1000

    Getting the user guide menu up and running

--- wp-content/themes/theblog-child/functions.php
+++ wp-content/themes/theblog-child/functions.php
@@ -1,3 +1,19 @@
 <?php
 	
 
+// Add Your Menu Locations
+function register_my_menus() {
+  register_nav_menus(
+    array(  
+      'user_guide' => __( 'User Guide' ), 
+    )
+  );
+} 
+
+add_action( 'init', 'register_my_menus' );
+
+function default_user_guide() { // HTML markup for a default message in menu location
+  echo "<ul class='nav'>          
+    <li>User Guide</li>
+  </ul>";
+}
