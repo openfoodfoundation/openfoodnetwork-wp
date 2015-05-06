9c62aa0d0bda90be702c2ff3e3cf495076726a9f
summerscope <summerscope@gmail.com>
2015-05-06 14:20:30 +1000

    Undo

--- wp-content/themes/theblog-child/functions.php
+++ wp-content/themes/theblog-child/functions.php
@@ -1,29 +1,3 @@
 <?php
 	
-// This is best practice stuff:	
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-	function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '../theblog/style.css' );
-    wp_enqueue_style( 'child-style',
-      get_stylesheet_directory_uri() . '../theblog-child/style.css',
-      array('parent-style')
-  );
-}
-
-// Add Your Menu Locations
-function register_my_menus() {
-  register_nav_menus(
-    array(  
-    	'user_guide' => __( 'User Guide' ), 
-    )
-  );
-} 
-
-add_action( 'init', 'register_my_menus' );
-
-function default_user_guide() { // HTML markup for a default message in menu location
-	echo "<ul class='nav'>					
-		<li>User Guide</li>
-	</ul>"
-}
 
